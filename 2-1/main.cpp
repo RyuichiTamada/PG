784751cb2da3eb758fee0ff31731f18201ab53ce
pipip <pipip@MSI>
2024-02-13 08:08:37 +0900

    -

--- 2-1/main.cpp
+++ 2-1/main.cpp
@@ -1,18 +1,70 @@
 #include <stdio.h>
-#include "IShape.h"
-#include "Circle.h"
-#include "Rectangle.h"
+#include <iostream>
+#include <Windows.h>
+#include <list>
 
-int main() {
+int main(void) {
+	SetConsoleOutputCP(65001);
 
-	IShape* isAhape_[2];
+	std::list<const char*> yamanoteStationName = {
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho",
+	};
 
-	isAhape_[0] = new Circle;
-	isAhape_[1] = new Rectangle;
+	std::cout << "1970”N‚ÌŽRŽèü‰wˆê——" << std::endl;
 
-	for (int i = 0; i < 2; i++) {
-		isAhape_[i]->Size();
-		isAhape_[i]->Draw();
+	for (auto& name : yamanoteStationName) {
+		std::cout << name << std::endl;
+	}
+
+	std::cout << "\n" << std::endl;
+
+	auto findStationNameIterator = std::find(yamanoteStationName.begin(), yamanoteStationName.end(), "Nippori");
+	findStationNameIterator++;
+	yamanoteStationName.insert(findStationNameIterator, "Nishi-Nippori");
+
+	std::cout << "2019”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+
+	for (auto& name : yamanoteStationName) {
+		std::cout << name << std::endl;
+	}
+
+	std::cout << "\n" << std::endl;
+
+	findStationNameIterator = std::find(yamanoteStationName.begin(), yamanoteStationName.end(), "Shinagawa");
+	findStationNameIterator++;
+	findStationNameIterator = yamanoteStationName.insert(findStationNameIterator, "Takanawa Gateway");
+
+	std::cout << "2022”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+
+	for (auto& name : yamanoteStationName) {
+		std::cout << name << std::endl;
 	}
 
 	return 0;
