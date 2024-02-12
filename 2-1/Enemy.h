9e951f796cc7c86f45cebe93a185e26ad6f1be14
pipip <pipip@MSI>
2024-02-12 23:49:59 +0900

    完了

--- 2-1/Enemy.h
+++ 2-1/Enemy.h
@@ -1,5 +1,42 @@
 #pragma once
+#include "Phase.h"
 class Enemy
 {
-};
+public:
 
+	/// <summary>
+	/// ?X?V
+	/// </summary>
+	void Update();
+
+
+	bool GetCount() { return count; }
+
+private:
+
+	/// <summary>
+	/// ????
+	/// </summary>
+	void Approach();
+
+	/// <summary>
+	/// ????
+	/// </summary>
+	void Shooting();
+
+	/// <summary>
+	/// ??ÅfE
+	/// </summary>
+	void Leave();
+
+
+
+private:
+
+	static void (Enemy::* fhaseTable[])();
+
+	Phase phase_ = Phase::APPROACH;
+
+	bool count = false;
+
+};
--- 2-1/Phase.h
+++ 2-1/Phase.h
@@ -1,1 +1,7 @@
 #pragma once
+
+enum class Phase {
+	APPROACH,
+	SHOOTING,
+	LEAVE
+};
